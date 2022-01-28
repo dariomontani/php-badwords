3cf60d8fbd1a6b1825b85608b2c403b008a8a7f6
dariomontani <dariomontani@gmail.com>
2022-01-28 15:07:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall’utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
+<!-- Ps provate una volta avviato MAMP a richiamare nel terminale il comando php -v  per sapere se sta funzionando php anche da terminale -->
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    
+
+</body>
+</html>
